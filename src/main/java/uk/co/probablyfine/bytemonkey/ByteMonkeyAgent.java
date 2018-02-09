c97708745f529b359bb300c945aea5d2220b8b31
GluckZhang <zhanglong3030@outlook.com>
2018-02-09 11:36:12 +0100

    feat: for already loaded classes, try to retransform them with byte-monkey

--- src/main/java/uk/co/probablyfine/bytemonkey/ByteMonkeyAgent.java
+++ src/main/java/uk/co/probablyfine/bytemonkey/ByteMonkeyAgent.java
@@ -8,6 +8,26 @@ public class ByteMonkeyAgent {
     public static void premain(String agentArguments, Instrumentation instrumentation) throws UnmodifiableClassException {
         ByteMonkeyClassTransformer transformer = new ByteMonkeyClassTransformer(agentArguments);
         instrumentation.addTransformer(transformer);
+
+        // for already loaded classes, we can retransform them, but that depends on platform's type
+        if (instrumentation.isRetransformClassesSupported()) {
+            Class cl[] = instrumentation.getAllLoadedClasses();
+            for (int i = 0; i < cl.length; i++) {
+                String className = cl[i].getName();
+                String prefix = className.split("//.")[0];
+                if (prefix.contains("java") || prefix.contains("sun") || prefix.startsWith("[") || className.contains("$")) {
+                    continue;
+                }
+
+                try {
+                    instrumentation.retransformClasses(cl[i]);
+                } catch (UnmodifiableClassException e){
+                    System.out.println("can't retransformClass: " + className);
+                }
+            }
+        } else {
+            System.out.println("WARN ByteMonkey: Retransforming classes is not supported!");
+        }
     }
 
     /* Duplicate of premain(), needed for ea-agent-loader in tests */
