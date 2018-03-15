077423a843fe4d65b8bae8940efbaa316bd1412b
GluckZhang <zhanglong3030@qq.com>
2018-03-15 12:03:25 +0100

    unified the message format in analyze mode

--- src/main/java/uk/co/probablyfine/bytemonkey/ChaosMonkey.java
+++ src/main/java/uk/co/probablyfine/bytemonkey/ChaosMonkey.java
@@ -1,8 +1,10 @@
 package uk.co.probablyfine.bytemonkey;
 
 import net.rubyeye.xmemcached.MemcachedClient;
-import net.rubyeye.xmemcached.XMemcachedClient;
+import net.rubyeye.xmemcached.MemcachedClientBuilder;
+import net.rubyeye.xmemcached.XMemcachedClientBuilder;
 import net.rubyeye.xmemcached.exception.MemcachedException;
+import net.rubyeye.xmemcached.utils.AddrUtil;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,10 +13,13 @@ import java.io.PrintWriter;
 import java.util.concurrent.TimeoutException;
 
 public class ChaosMonkey {
+    public static MemcachedClient memcachedClient = null;
+
     public static void doChaos(String tcIndexInfo, String tcType, String defaultMode, String memcachedHost, int memcachedPort) throws Throwable {
         String chaosMode = getMode(tcIndexInfo, memcachedHost, memcachedPort);
         if (chaosMode == null) {
-            System.out.println("INFO ByteMonkey unregistered try catch found, use default mode: " + defaultMode);
+            System.out.println("INFO ByteMonkey unregistered try catch found, register and use default mode: " + defaultMode);
+            registerTrycatchToMemcached(tcIndexInfo, defaultMode, memcachedHost, memcachedPort);
             chaosMode = defaultMode;
         }
         if (chaosMode.equals("analyze")) {
@@ -32,9 +37,12 @@ public class ChaosMonkey {
         String mode = null;
 
         try {
-            MemcachedClient client = new XMemcachedClient(memcachedHost,memcachedPort);
-            mode = client.get(tcIndexInfo, 1000);
-            client.shutdown();
+            if (memcachedClient == null) {
+                MemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(memcachedHost + ":" + memcachedPort));
+                builder.setConnectionPoolSize(5);
+                memcachedClient = builder.build();
+            }
+            mode = memcachedClient.get(tcIndexInfo, 1000);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -42,6 +50,7 @@ public class ChaosMonkey {
         } catch (MemcachedException e) {
             e.printStackTrace();
         } catch (TimeoutException e) {
+            mode = "UNKNOWN";
             String executedClassName = Thread.currentThread().getStackTrace()[3].getClassName();
             String executedMethodName = Thread.currentThread().getStackTrace()[3].getMethodName();
             System.out.println(String.format("INFO ByteMonkey getMode time out, %s @ %s", executedMethodName, executedClassName));
@@ -51,12 +60,7 @@ public class ChaosMonkey {
     }
 
     public static void printInfo(String tcIndexInfo) {
-        String executedClassName = Thread.currentThread().getStackTrace()[3].getClassName();
-        String executedMethodName = Thread.currentThread().getStackTrace()[3].getMethodName();
-
-        // TryCatch Info
-        System.out.println(String.format("INFO ByteMonkey try catch index %s, %s @ %s", tcIndexInfo, executedMethodName, executedClassName));
-        System.out.println("----");
+        System.out.println(String.format("INFO ByteMonkey try catch index %s", tcIndexInfo));
     }
 
     public static Throwable throwOrDefault(String tcIndexInfo, String tcType) {
@@ -131,5 +135,24 @@ public class ChaosMonkey {
             System.out.println(String.format("INFO ByteMonkey registerTrycatchInfo time out (%s)", memcachedKey));
         }
         */
+    }
+
+    public static void registerTrycatchToMemcached(String tcIndexInfo, String defaultMode, String memcachedHost, int memcachedPort) {
+        try {
+            if (memcachedClient == null) {
+                MemcachedClientBuilder builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(memcachedHost + ":" + memcachedPort));
+                builder.setConnectionPoolSize(5);
+                memcachedClient = builder.build();
+            }
+            memcachedClient.set(tcIndexInfo, 0, defaultMode);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (MemcachedException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            System.out.println(String.format("INFO ByteMonkey registerTrycatchToMemcached time out (%s)", tcIndexInfo));
+        }
     }
 }
